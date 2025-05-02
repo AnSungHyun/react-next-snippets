385fd43c653269c8a1fb3952ba02debfbeb4ab49
AnSungHyun <ash10264@gmail.com>
2025-05-02 14:01:05 +0900

    "Rewrites 설정에 header 조건 추가하여 page와 API 요청 분리"

--- next.config.ts
+++ next.config.ts
@@ -19,6 +19,15 @@ const nextConfig: NextConfig = {
         // rewirtes 설정을 통해 cors 문제 해결 및 backend api 주소 숨김
         source: '/:path*', // 클라이언트에서 요청할 경로
         destination: 'https://dummyjson.com/:path*', // 실제 백엔드 API 주소
+        // page 요청과 API 요청을 구분하기 위한 조건
+        has: [
+          {
+            type: 'header',
+            key: 'accept',
+            value: '(.*application/json.*|.*text/json.*)',
+          },
+        ]
+
       },
       // {
       //   source: '/products/:path*', // products로 시작하는 모든 경로
