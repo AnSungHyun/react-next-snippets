d32a5eec2eaa3d3d2b5f6cd02a0e67967c347ecf
AnSungHyun <ash10264@gmail.com>
2025-05-15 02:02:54 +0900

    비동기 처리 도입 및 파라미터 처리 방식 수정

--- src/app/(pages)/snippets/52/[slug]/page.tsx
+++ src/app/(pages)/snippets/52/[slug]/page.tsx
@@ -1,17 +1,20 @@
 // app/posts/[slug]/page.tsx
-export default function ProductPage({
+export default async function ProductPage({
                                    params,
                                    searchParams,
                                  }: {
   params: { slug: string };
   searchParams: { [key: string]: string | string[] | undefined };
 }) {
+  const slugParams = await Promise.resolve(params);
+  const resolvedSearchParams = await Promise.resolve(searchParams);
+
   return (
     <div>
       <h1>상품 페이지</h1>
 
-      <p>슬러그: {params.slug}</p>
-      <p>검색 파라미터: {JSON.stringify(searchParams)}</p>
+      <p>슬러그: {slugParams.slug}</p>
+      <p>검색 파라미터: {JSON.stringify(resolvedSearchParams)}</p>
     </div>
   );
 }
--- src/app/(pages)/snippets/52/serverSearchParams/page.tsx
+++ src/app/(pages)/snippets/52/serverSearchParams/page.tsx
@@ -1,14 +1,15 @@
 // app/search/page.tsx
-export default function SearchPage({
+export default async function SearchPage({
                                      searchParams,
                                    }: {
   searchParams: { query?: string; page?: string };
 }) {
+  const resolvedSearchParams = await Promise.resolve(searchParams);
   return (
     <div>
       <h1>검색 결과</h1>
-      <p>검색어: {searchParams.query}</p>
-      <p>페이지: {searchParams.page || '1'}</p>
+      <p>검색어: {resolvedSearchParams.query}</p>
+      <p>페이지: {resolvedSearchParams.page || '1'}</p>
     </div>
   );
 }
